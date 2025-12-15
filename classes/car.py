92eb5aecd2c37d7ba571c03911115467eca27cc9
lbalbino <lbalbino.dev@gmail.com>
2025-12-15 22:50:23 +0000

    class implementations

--- classes/car.py
+++ classes/car.py
@@ -1,8 +1,13 @@
 class Carro:
-   def __init__(self, marca, modelo, ano):
+   def __init__(self, marca, modelo, ano, categoria, revisao):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
+        self.categoria = categoria
+        self.disponivel = True
+        self.data_revisao = revisao
+
+
 
    def exibir_informacoes(self):
         return f"{self.ano} {self.marca} {self.modelo}"
@@ -15,5 +20,7 @@ class Carro:
 
    def atualizar_marca(self, nova_marca):
       self.marca = nova_marca
-    
-   def 
+
+   def marcar_alugado(self):
+      self.disponivel = False
+   
