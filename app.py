33a334b8c8031fff5e32f2807599319ee967370f
lbalbino <lbalbino.dev@gmail.com>
2025-12-17 00:07:04 +0000

    Login implementation complete

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 from flask import Flask, render_template, url_for, redirect, request
-from flask_login import UserMixin
-import flask_sqlalchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_wtf import FlaskForm
@@ -15,14 +14,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'database.db')}"
 app.config['SECRET_KEY'] = 'LuxuryWheelsSecretKey'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+   return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(20), nullable=False, unique=True)
    password = db.Column(db.String(50), nullable=False)
-
-
 
 
 class RegisterForm(FlaskForm):
@@ -51,9 +58,15 @@ class LoginForm(FlaskForm):
 
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
    form = LoginForm()
+   if form.validate_on_submit():
+       user = User.query.filter_by(username=form.username.data).first()
+       if user and bcrypt.check_password_hash(user.password, form.password.data):
+           login_user(user)
+           return redirect('dashboards')
+
    return render_template('login.html', form=form)
 
 
@@ -65,8 +78,9 @@ def  register():
    form = RegisterForm()
     
 
+
    if request.method == 'POST': 
-         hashed_password = Bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
          new_user = User(username=form.username.data, password=hashed_password)
          db.session.add(new_user)
          db.session.commit()
@@ -77,11 +91,14 @@ def  register():
 
 
 @app.route('/dashboards')
+@login_required
 def home():
    return render_template('dashboards.html')
 
-
-
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+   logout_user()
+   return redirect(url_for('login'))
 
 
 
